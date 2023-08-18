ad8fdb3f67e6630bc292759c95c29338de379df6
Ali Sadeghi <ali909392@gmail.com>
2023-08-19 00:18:26 +0330

    Use main project adrs for urls / Organize structure of the project / Removed max limitations of the project

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.body.style.transition = "box-shadow 0.2s";
 
 function updateBorder() {
-    const minWidth = 1130;
-    const maxWidth = 1715;
+    const minWidth = 1090;
     const currentWidth = window.innerWidth;
 
-    if (currentWidth < minWidth || currentWidth > maxWidth) {
+    if (currentWidth < minWidth) {
         document.body.style.boxShadow = "inset 0 0 10px 10px red";
     } else {
         document.body.style.boxShadow = "inset 0 0 10px 10px transparent";
